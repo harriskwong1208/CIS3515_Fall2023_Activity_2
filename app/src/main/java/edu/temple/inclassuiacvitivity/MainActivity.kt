a3ef19f9786349738430130ec63ac7c337a1d23d
harriskwong1208 <harriskwong120898@gmail.com>
2023-02-09 12:19:39 -0500

    Finished activity 3.

--- app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/MainActivity.kt
@@ -3,6 +3,7 @@ package edu.temple.inclassuiacvitivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.webkit.WebSettings.TextSize
 import android.widget.*
 import android.widget.AdapterView.OnItemSelectedListener
 import java.util.*
@@ -21,7 +22,8 @@ class MainActivity : AppCompatActivity() {
 
 
         /* Step 2: Create adapter to display items from array in Spinner */
-        spinner.adapter = ArrayAdapter(this,android.R.layout.simple_dropdown_item_1line,numberArray)
+        //spinner.adapter = ArrayAdapter(this,android.R.layout.simple_dropdown_item_1line,numberArray)
+        spinner.adapter = TextSizeAdapter(this,numberArray)
 
 
         // Step 3: Change TextView's text size to the number selected in the Spinner */
--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -6,7 +6,7 @@ import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.TextView
 
-class TextSizeAdapter(_context: Context, _textSizes: Array<Int>) : BaseAdapter(){
+class TextSizeAdapter(_context: Context, _textSizes : Array<Int>) : BaseAdapter(){
 
     val context = _context
     val textSizes = _textSizes
@@ -24,8 +24,24 @@ class TextSizeAdapter(_context: Context, _textSizes: Array<Int>) : BaseAdapter()
     }
 
     override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
-        val textView = TextView(context)
-        textView.text = textSizes[p0].toString()
+
+        return (getDropDownView(p0,p1,p2) as TextView).apply{
+            textSize = 22f
+        }
+    }
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val textView: TextView
+
+        if(position == null){
+            textView = TextView(context)
+            textView.setPadding(10,20,0,20)
+        }
+        else{
+            textView = convertView as TextView
+        }
+        textView.textSize = textSizes[position].toFloat()
+        textView.text = textSizes[position].toString()
 
         return textView
     }

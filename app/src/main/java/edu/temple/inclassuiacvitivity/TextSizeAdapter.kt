2e03de4b9e24609d71145081829876050c606fe8
harriskwong1208 <harriskwong120898@gmail.com>
2023-02-09 11:43:09 -0500

    Basic adapter implementation.

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -1,24 +1,33 @@
 package edu.temple.inclassuiacvitivity
 
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
 
-class TextSizeAdapter : BaseAdapter(){
+class TextSizeAdapter(_context: Context, _textSizes: Array<Int>) : BaseAdapter(){
+
+    val context = _context
+    val textSizes = _textSizes
+
     override fun getCount(): Int {
-        TODO("Not yet implemented")
+       return textSizes.size
     }
 
     override fun getItem(p0: Int): Any {
-        TODO("Not yet implemented")
+        return textSizes[p0]
     }
 
     override fun getItemId(p0: Int): Long {
-        TODO("Not yet implemented")
+        return p0.toLong()
     }
 
     override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
-        TODO("Not yet implemented")
+        val textView = TextView(context)
+        textView.text = textSizes[p0].toString()
+
+        return textView
     }
 
 }
